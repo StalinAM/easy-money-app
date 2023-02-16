b6f8d11ce6650c346165b236a5c199130411630e
Stalin <sta_vla411@hotmail.com>
2023-02-15 23:51:55 -0500

    add sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
-import SignIn from "./components/auth/SignIn";
+import { useState } from 'react'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className='App'>
       <SignIn />
+      <SignUp />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
