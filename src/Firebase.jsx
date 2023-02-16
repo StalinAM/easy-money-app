32e322b9a10cb36f4b3a5141a37f5a96a50c5d55
Stalin <sta_vla411@hotmail.com>
2023-02-15 23:52:17 -0500

    refactor with eslint

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -1,20 +1,20 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBs53ClahpeFzoismFmn5C2br1Kd2I07gw",
-  authDomain: "easy-money-621ad.firebaseapp.com",
-  projectId: "easy-money-621ad",
-  storageBucket: "easy-money-621ad.appspot.com",
-  messagingSenderId: "7159857453",
-  appId: "1:7159857453:web:2a48e817fb038939de13a4",
-};
+  apiKey: 'AIzaSyBs53ClahpeFzoismFmn5C2br1Kd2I07gw',
+  authDomain: 'easy-money-621ad.firebaseapp.com',
+  projectId: 'easy-money-621ad',
+  storageBucket: 'easy-money-621ad.appspot.com',
+  messagingSenderId: '7159857453',
+  appId: '1:7159857453:web:2a48e817fb038939de13a4'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+export const auth = getAuth(app)
--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,35 +1,37 @@
-import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../../Firebase'
 function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   const signIn = (e) => {
-    e.preventDefault();
+    e.preventDefault()
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => console.log(userCredential))
       .catch((error) => {
-        console.log(error);
-      });
-  };
+        console.log(error)
+      })
+  }
   return (
     <div>
       <form onSubmit={signIn}>
+        <h1>Login In</h1>
         <input
-          type="email"
-          placeholder="Ingresa un correo"
+          type='email'
+          placeholder='Ingresa un correo'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          type="password"
-          placeholder="Ingresa una contraseña"
+          type='password'
+          placeholder='Ingresa una contraseña'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Log in</button>
+        <button type='submit'>Log in</button>
       </form>
     </div>
-  );
+  )
 }
 
-export default SignIn;
+export default SignIn
