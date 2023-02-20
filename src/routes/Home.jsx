0ceb32a57edf45cab7ff3d4f1a76d5646b53298c
Stalin <sta_vla411@hotmail.com>
2023-02-19 23:38:42 -0500

    add main style

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 import HeaderHome from '../components/HeaderHome'
 import MainContent from '../components/MainContent'
-
+const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  padding: 0 6rem;
+  gap: 6rem;
+`
 function Home() {
   return (
     <>
       <HeaderHome />
-      <main>
+      <MainContainer>
         <MainContent />
-      </main>
+      </MainContainer>
     </>
   )
 }
