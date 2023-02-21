951dc838e1b90dd981fb8bb4b9d27d5b91e2c9ef
Stalin <sta_vla411@hotmail.com>
2023-02-20 19:31:58 -0500

    add user context

---
+++ src/context/Auth.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { createContext, useEffect, useState } from 'react'
+import { auth } from '../firebase/firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+
+export const AuthContext = createContext()
+
+function Auth({ children }) {
+  const [currentUser, setCurrentUser] = useState({})
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user)
+    })
+
+    return () => {
+      unsub()
+    }
+  }, [])
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default Auth
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import Auth from './context/Auth'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <Auth>
+      <App />
+    </Auth>
   </BrowserRouter>
 )
