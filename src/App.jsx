6c734857036776e75d140fc26dc2fc0d647ae4e7
Stalin <sta_vla411@hotmail.com>
2023-02-20 19:31:46 -0500

    add protect route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { dark } from './style/Theme'
 import GlobalStyles from './style/GlobalStyles'
+import { useContext } from 'react'
 
 import Home from './routes/Home'
 import Dashboard from './routes/Dashboard'
 import Profile from './routes/Profile'
 import SignIn from './routes/SignIn'
 import SignUp from './routes/SignUp'
+import { AuthContext } from './context/Auth'
 
 function App() {
+  const { currentUser } = useContext(AuthContext)
+  const ProtectRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to='/signin' />
+    }
+    return children
+  }
   return (
     <>
       <GlobalStyles />
@@ -18,7 +27,14 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='signin' element={<SignIn />} />
           <Route path='signup' element={<SignUp />} />
-          <Route path='dashboard' element={<Dashboard />} />
+          <Route
+            path='dashboard'
+            element={
+              <ProtectRoute>
+                <Dashboard />
+              </ProtectRoute>
+            }
+          />
           <Route path='u/:username' element={<Profile />} />
         </Routes>
       </ThemeProvider>
