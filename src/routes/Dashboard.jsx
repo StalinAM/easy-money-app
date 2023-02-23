4caf1da052a293c19d262a7ad2b1197af68dadd9
Stalin <sta_vla411@hotmail.com>
2023-02-22 23:12:08 -0500

    add log out btn

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import logo from '../assets/logo.svg'
+import Button from '../components/Button'
+import { logoutUser } from '../firebase/services'
 const Container = styled.main`
   width: 100%;
   min-height: 100vh;
@@ -11,11 +13,14 @@ const Container = styled.main`
   background-color: ${(props) => props.theme.blue};
 `
 const Menu = styled.aside`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   background-color: ${(props) => props.theme.white};
   border-radius: 12px;
   padding: 1.5rem 1rem;
   img {
-    margin: 0 auto;
     width: 15rem;
     filter: invert(22%) sepia(59%) saturate(4758%) hue-rotate(213deg)
       brightness(105%) contrast(103%);
@@ -24,11 +29,20 @@ const Menu = styled.aside`
 const Navbar = styled.nav`
   grid-column: 2/5;
 `
+
+const handleClick = () => {
+  logoutUser()
+}
 function Dashboard() {
   return (
     <Container>
       <Menu>
         <img src={logo} alt='logo of easymoney' />
+        <Button
+          handleClick={handleClick}
+          text='Cerrar sessión'
+          link='/signin'
+        />
       </Menu>
       <Navbar></Navbar>
     </Container>
