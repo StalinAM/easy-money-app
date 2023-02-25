af9fa15c4484c126cc03ba72030652a5e5791b13
Stalin <sta_vla411@hotmail.com>
2023-02-24 19:36:05 -0500

    add total information component

---
+++ src/components/TotalInfo.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.article`
+  position: relative;
+  width: 100%;
+  background-color: ${(props) => props.theme.white};
+  border-radius: 12px;
+  padding: 2.5rem 1.5rem;
+  text-align: right;
+  h2 {
+    font-size: 2.4rem;
+    font-weight: bold;
+    color: ${(props) => props.theme.darkBlue};
+  }
+`
+const Icon = styled.i`
+  position: absolute;
+  color: ${(props) => props.theme.white};
+  background-color: ${(props) => props.color};
+  padding: 0.938rem 1.563rem;
+  font-size: 2.5rem;
+  border-radius: 12px;
+  top: 0;
+  left: 1.5rem;
+  transform: translateY(-50%);
+`
+function TotalInfo({ icon, color, title, value }) {
+  return (
+    <Container>
+      <Icon color={color} className={`uil uil-money-${icon}`} />
+      <p>{title}</p>
+      <h2>${value}</h2>
+    </Container>
+  )
+}
+
+export default TotalInfo
