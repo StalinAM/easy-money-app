9413baa70e3d2e036fe3e1e454ff1ae61e378d5a
Stalin <sta_vla411@hotmail.com>
2023-03-01 11:22:17 -0500

    add datepicker from mui-x

--- src/components/NewItem.jsx
+++ src/components/NewItem.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Input } from '../style/StyleComponents'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { Box } from '@mui/material'
+import 'dayjs/locale/es'
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -18,7 +23,29 @@ const ModalC = styled.div`
   position: relative;
   border-radius: 30px;
   width: 425px;
-  padding: 5.5rem 0 3rem;
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  i {
+    position: absolute;
+    top: 1rem;
+    right: 2rem;
+    color: ${(props) => props.theme.blue};
+    font-size: 3rem;
+    cursor: pointer;
+  }
+`
+const FormC = styled.form`
+  display: flex;
+  flex-direction: column;
+  max-width: 350px;
+  gap: 1.125rem;
+  margin: 0 auto;
+`
+const DateC = styled(DatePicker)`
+  border: none;
+  color: green;
 `
 function NewItem({ active, setActive }) {
   const [date, setDate] = useState('')
@@ -30,14 +57,40 @@ function NewItem({ active, setActive }) {
         <Container>
           <ModalC>
             <i onClick={() => setActive(!active)} className='uil uil-times' />
-            <form action=''>
+            <FormC action=''>
+              <LocalizationProvider
+                adapterLocale={'es'}
+                dateAdapter={AdapterDayjs}
+              >
+                <DateC
+                  label='Basic example'
+                  value={date}
+                  onChange={(newValue) => {
+                    setDate(newValue)
+                  }}
+                  renderInput={({ inputRef, inputProps, InputProps }) => (
+                    <Box
+                      sx={{ display: 'flex', alignItems: 'center', gap: '0' }}
+                    >
+                      <Input ref={inputRef} {...inputProps} />
+                      {InputProps?.endAdornment}
+                    </Box>
+                  )}
+                />
+              </LocalizationProvider>
               <Input
-                type='text '
+                type='text'
                 placeholder='Ingresa una descripcion'
                 value={description}
                 onChange={(e) => setDescription(e.target.value)}
               />
-            </form>
+              <Input
+                type='text'
+                placeholder='Ingresa el valor'
+                value={income}
+                onChange={(e) => setIncome(e.target.value)}
+              />
+            </FormC>
           </ModalC>
         </Container>
       )}
