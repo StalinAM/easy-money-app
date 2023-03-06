e5cd97956cfa4d2564f54b896937df23879292e9
Stalin <sta_vla411@hotmail.com>
2023-03-05 20:35:04 -0500

    add context transaction

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import Auth from './context/Auth'
+import TransactionsContext from './context/TransactionsContext'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Auth>
-      <App />
+      <TransactionsContext>
+        <App />
+      </TransactionsContext>
     </Auth>
   </BrowserRouter>
 )
