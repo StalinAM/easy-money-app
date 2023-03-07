c0029df05c9eafc09c4c3875d3cdad174c9379e6
Stalin <sta_vla411@hotmail.com>
2023-03-07 16:49:55 -0500

    saparate menu to component

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,33 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import logo from '../assets/logo.svg'
-import Button from '../components/Button'
 import TotalInfo from '../components/TotalInfo'
-import { logoutUser } from '../firebase/services'
 import Profile from '../components/Profile'
 import Table from '../components/Table'
+import Menu from '../components/Menu'
 
 function Dashboard() {
-  const handleClick = () => {
-    logoutUser()
-  }
   return (
     <Container>
-      <Menu>
-        <div>
-          <img src={logo} alt='logo of easymoney' />
-          <br />
-          <MenuRoutes>
-            <li>General</li>
-            <li>Tablas</li>
-          </MenuRoutes>
-        </div>
-        <Button
-          handleClick={handleClick}
-          text='Cerrar sessión'
-          link='/signin'
-        />
-      </Menu>
+      <Menu />
       <Profile />
       <ContainerTotal>
         <TotalInfo icon='stack' title='Valor total' value='130' />
@@ -49,22 +30,8 @@ const Container = styled.main`
   display: grid;
   gap: 2rem;
   grid-template-columns: 300px 1fr;
-  grid-template-rows: 100px 1fr 1fr 1fr;
+  grid-template-rows: 70px 1fr 1fr 1fr;
   background-color: ${(props) => props.theme.blue};
-`
-const Menu = styled.aside`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  grid-row: 1/5;
-  align-items: center;
-  background-color: ${(props) => props.theme.darkLightBlue};
-  border-radius: 12px;
-  padding: 1.5rem 1rem;
-  box-shadow: #00000021 0px 1px 9px 0px;
-  img {
-    width: 15rem;
-  }
 `
 
 const ContainerTotal = styled.div`
@@ -72,10 +39,4 @@ const ContainerTotal = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 2rem;
-`
-
-const MenuRoutes = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `
