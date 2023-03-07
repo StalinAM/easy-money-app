0fa4710c9f0f14862c37cef34d602ff709d07454
Stalin <sta_vla411@hotmail.com>
2023-03-07 16:47:19 -0500

    remove date input

--- src/components/FormTransaction.jsx
+++ src/components/FormTransaction.jsx
@@ -1,20 +1,14 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Input } from '../style/StyleComponents'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
-import { DatePicker } from '@mui/x-date-pickers/DatePicker'
-import { Box } from '@mui/material'
 import 'dayjs/locale/es'
 function FormTransaction({
   handleSubmit,
   active,
   setActive,
   transaction,
-  setTransaction,
-  setDate
+  setTransaction
 }) {
-  const [dateInput, setDateInput] = useState(null)
   const handleInputChange = (e) => {
     const { name, value } = e.target
     setTransaction((prevState) => ({
@@ -27,23 +21,6 @@ function FormTransaction({
       <ModalC>
         <i onClick={() => setActive(!active)} className='uil uil-times' />
         <FormC onSubmit={handleSubmit}>
-          <LocalizationProvider adapterLocale={'es'} dateAdapter={AdapterDayjs}>
-            <DatePicker
-              value={dateInput}
-              inputFormat='DD/MM/YYYY'
-              onChange={(newDate) => {
-                setDateInput(newDate)
-                setDate(newDate.$d.toLocaleString('es-CL').slice(0, -10))
-              }}
-              disableMaskedInput={true}
-              renderInput={({ inputRef, inputProps, InputProps }) => (
-                <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                  <Input ref={inputRef} {...inputProps} />
-                  {InputProps?.endAdornment}
-                </Box>
-              )}
-            />
-          </LocalizationProvider>
           <Input
             name='description'
             type='text'
@@ -51,20 +28,26 @@ function FormTransaction({
             value={transaction.description}
             onChange={handleInputChange}
           />
-          <Input
-            name='expense'
-            type='number'
-            placeholder='Ingresa el valor del gasto'
-            value={transaction.expense}
-            onChange={handleInputChange}
-          />
-          <Input
-            name='income'
-            type='number'
-            placeholder='Ingresa el valor del ingreso'
-            value={transaction.income}
-            onChange={handleInputChange}
-          />
+          <div>
+            <label for='income'>Valor del Ingreso</label>
+            <Input
+              name='income'
+              type='number'
+              placeholder='Ingresa el valor del ingreso'
+              value={transaction.income}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label for='expense'>Valor del Gasto</label>
+            <Input
+              name='expense'
+              type='number'
+              placeholder='Ingresa el valor del gasto'
+              value={transaction.expense}
+              onChange={handleInputChange}
+            />
+          </div>
           <SubmitBtn type='submit'>Añadir</SubmitBtn>
         </FormC>
       </ModalC>
@@ -92,7 +75,6 @@ const ModalC = styled.div`
   border-radius: 30px;
   width: 425px;
   padding: 2rem;
-  display: flex;
   align-items: center;
   justify-content: center;
   i {
@@ -107,7 +89,7 @@ const ModalC = styled.div`
 const FormC = styled.form`
   display: flex;
   flex-direction: column;
-  max-width: 350px;
+  max-width: 300px;
   gap: 1.125rem;
   margin: 0 auto;
 `
@@ -120,5 +102,13 @@ const SubmitBtn = styled.button`
   font-size: ${(props) => props.theme.mFont};
   &:hover {
     background-color: ${(props) => props.theme.lightBlue};
+  }
+`
+const ContainerValues = styled.div`
+  gap: 1rem;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
   }
 `
