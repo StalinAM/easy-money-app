b23f9a5ebce21df3cbd75b3c0f8022e8cf10260b
Stalin <sta_vla411@hotmail.com>
2023-03-07 23:26:54 -0500

    add button create new transaction and table

--- src/routes/Tables.jsx
+++ src/routes/Tables.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Menu from '../components/Menu'
+import Table from '../components/Table'
+import Transactions from '../components/Transactions'
 
 function Tables() {
+  const [active, setActive] = useState(false)
   return (
     <Container>
-      <Menu />
+      <Menu row='1/3' />
+      <ContainerBtn>
+        <BtnModal onClick={() => setActive(!active)}>Crear nuevo</BtnModal>
+      </ContainerBtn>
+      <Transactions active={active} setActive={setActive} />
+      <Table title='Transacciones' row='2/3' />
     </Container>
   )
 }
@@ -19,6 +27,20 @@ const Container = styled.main`
   display: grid;
   gap: 2rem;
   grid-template-columns: 300px 1fr;
-  grid-template-rows: 70px 1fr 1fr 1fr;
+  grid-template-rows: 70px 1fr;
   background-color: ${(props) => props.theme.blue};
+`
+const ContainerBtn = styled.div`
+  padding: 2rem 0;
+`
+const BtnModal = styled.button`
+  padding: 0.5rem 1rem;
+  border-radius: 12px;
+  font-size: ${(props) => props.theme.mFont};
+  background-color: ${(props) => props.theme.lightBlue};
+  color: ${(props) => props.theme.white};
+  box-shadow: #00000021 0px 1px 9px 0px;
+  &:hover {
+    border: 1px solid ${(props) => props.theme.white};
+  }
 `
