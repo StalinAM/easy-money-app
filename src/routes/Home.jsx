ca60fa2ee7e597d5eb8268c6f19e229085297ae1
Stalin <sta_vla411@hotmail.com>
2023-03-13 09:40:43 -0500

    remove padding of main

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Features from '../components/Features'
 import HeaderHome from '../components/HeaderHome'
 import MainContent from '../components/MainContent'
 
@@ -9,6 +10,7 @@ function Home() {
       <HeaderHome />
       <MainContainer>
         <MainContent />
+        <Features />
       </MainContainer>
     </>
   )
@@ -19,16 +21,4 @@ export default Home
 const MainContainer = styled.main`
   display: flex;
   flex-direction: column;
-  padding: 0 6rem;
-  gap: 6rem;
-  @media screen and (max-width: 1080px) {
-    padding: 0 3rem;
-    gap: 3rem;
-  }
-  @media screen and (max-width: 768px) {
-    padding: 0 1.5rem;
-  }
-  @media screen and (max-width: 480px) {
-    padding: 0 1rem;
-  }
 `
