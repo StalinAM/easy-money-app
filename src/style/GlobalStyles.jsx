6f1b08bd03ee737503dad368b30cec750c795710
Stalin <sta_vla411@hotmail.com>
2023-03-13 09:40:56 -0500

    update css

--- src/style/GlobalStyles.jsx
+++ src/style/GlobalStyles.jsx
@@ -23,15 +23,6 @@ const GlobalStyles = createGlobalStyle`
     header{
         padding: 3rem 6rem;
     }
-    section{
-        padding: 6rem 0 2rem;
-        @media screen and (max-width: 1080px) {
-            padding: 3rem 0 2rem;
-        }
-        @media screen and (max-width: 768px) {
-            padding: 1.5rem 0 2rem;
-        }
-    }
     article{
         max-width: 75rem;
         margin: 0 auto;
--- src/style/StyleComponents.jsx
+++ src/style/StyleComponents.jsx
@@ -23,3 +23,20 @@ export const Line = styled.hr`
   border-top: 2px solid ${(props) => props.theme.white};
   width: 100%;
 `
+export const SectionC = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+  position: relative;
+  padding: 4rem 6rem 2rem;
+  min-height: 100vh;
+  @media screen and (max-width: 1080px) {
+    padding: 3rem 3rem 2rem;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 1.5rem 1.5rem 2rem;
+  }
+  @media screen and (max-width: 480px) {
+    padding: 1rem 1rem 2rem;
+  }
+`
