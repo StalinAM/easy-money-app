44eea4e99a5d18f6ab73d968b5469540d35da741
Stalin <sta_vla411@hotmail.com>
2023-03-16 14:06:32 -0500

    add responsive menu

--- src/components/HeaderHome.jsx
+++ src/components/HeaderHome.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import logo from '../assets/logo.svg'
 
 function HeaderHome() {
+  const [active, setActive] = useState(false)
   return (
     <header>
       <Navbar>
         <Logo src={logo} alt='logo of easymoney' />
-        <List>
-          <Item>
+        <IconOpen onClick={() => setActive(true)} className='uil uil-bars' />
+        <List active={active}>
+          <IconClose
+            onClick={() => setActive(false)}
+            className='uil uil-times'
+          />
+          <Item onClick={() => setActive(false)}>
             <a href='#features'>Características</a>
           </Item>
-          <Item>
+          <Item onClick={() => setActive(false)}>
             <a href='#getstarted'>Empezar</a>
           </Item>
         </List>
@@ -33,8 +39,24 @@ const Logo = styled.img`
   width: 13rem;
 `
 const List = styled.ul`
+  position: relative;
   display: flex;
+  align-items: center;
   gap: 1.5rem;
+  @media screen and (max-width: 768px) {
+    transform: ${(props) =>
+      props.active ? 'translateX(0)' : 'translateX(100%)'};
+    position: fixed;
+    height: 100vh;
+    right: 0;
+    top: 0;
+    z-index: 100;
+    transition: transform 0.5s ease-in-out;
+    background-color: ${(props) => props.theme.lightBlue};
+    flex-direction: column;
+    justify-content: center;
+    width: min(75vw, 300px);
+  }
 `
 const Item = styled.li`
   cursor: pointer;
@@ -52,5 +74,26 @@ const Item = styled.li`
   }
   &:hover::before {
     width: 100%;
+  }
+`
+const IconClose = styled.i`
+  display: none;
+  color: ${(props) => props.theme.white};
+  font-size: 3rem;
+  cursor: pointer;
+  @media screen and (max-width: 780px) {
+    display: block;
+    position: absolute;
+    top: 2.5rem;
+    right: 1rem;
+  }
+`
+const IconOpen = styled.i`
+  display: none;
+  cursor: pointer;
+  color: ${(props) => props.theme.white};
+  font-size: ${(props) => props.theme.xlFont};
+  @media screen and (max-width: 768px) {
+    display: block;
   }
 `
