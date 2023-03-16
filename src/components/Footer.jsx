324f56de165203bc87defca2c835bda965f7221f
Stalin <sta_vla411@hotmail.com>
2023-03-16 14:05:15 -0500

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styled from 'styled-components'
+import logo from '../assets/logo-symbol.svg'
+
+function Footer() {
+  return (
+    <Container>
+      <img src={logo} alt='' />
+      <p>
+        Desarrollado por{' '}
+        <Creator href='https://github.com/StalinAM'>Stalin</Creator>
+      </p>
+    </Container>
+  )
+}
+
+export default Footer
+
+const Container = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3rem 6rem;
+  gap: 1rem;
+  color: ${(props) => props.theme.white};
+  img {
+    width: 5rem;
+  }
+  @media screen and (max-width: 768px) {
+    padding: 3rem 1.5rem;
+  }
+  @media screen and (max-width: 480px) {
+    padding: 2rem 1rem;
+  }
+`
+
+const Creator = styled.a`
+  font-weight: bold;
+  &:hover {
+    color: ${(props) => props.theme.lightBlue};
+  }
+`
