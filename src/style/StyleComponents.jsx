532bbaf7383a00d8a0dfba74e5116679204ddac2
Stalin <sta_vla411@hotmail.com>
2023-04-20 14:57:00 -0500

    change colors

--- src/style/StyleComponents.jsx
+++ src/style/StyleComponents.jsx
@@ -2,20 +2,19 @@ import styled from 'styled-components'
 
 export const Input = styled.input`
   width: 100%;
-  border: 2px solid ${(props) => props.theme.xLightBlue};
+  border: 2px solid ${(props) => props.theme.black_400};
   padding: 0.75rem 1rem;
   border-radius: 12px;
   background: none;
-  color: ${(props) => props.theme.darkBlue};
+  color: ${(props) => props.theme.black_500};
   font-weight: 400;
   font-size: ${(props) => props.theme.mFont};
   &::placeholder {
-    color: ${(props) => props.theme.gray};
+    color: ${(props) => props.theme.black_400};
   }
 `
 export const LabelInput = styled.label`
-  color: ${(props) => props.theme.darkLightBlue};
-  font-weight: bold;
+  color: ${(props) => props.theme.black_500};
 `
 export const Line = styled.hr`
   border: none;
