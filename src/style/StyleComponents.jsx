307d8d6a1bea4c2016efc33a195a58a7a7f36111
Stalin <sta_vla411@hotmail.com>
2023-04-21 09:47:46 -0500

    add container and content of section

--- src/style/StyleComponents.jsx
+++ src/style/StyleComponents.jsx
@@ -22,3 +22,33 @@ export const Line = styled.hr`
   border-top: 4px solid ${(props) => props.theme.black_300};
   width: 100%;
 `
+export const Container = styled.article`
+  display: flex;
+  gap: 4rem;
+  flex-wrap: wrap-reverse;
+  align-items: center;
+  justify-content: center;
+  img {
+    max-width: 30rem;
+    border-radius: 12px;
+    border: 2px solid ${props=>props.theme.black_500};
+    @media screen and (max-width: 768px) {
+      max-width: 100%;
+    }
+  }
+`
+export const Content = styled.div`
+  max-width: 50ch;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  h3 {
+    font-size: ${(props) => props.theme.xlFont};
+    color: ${(props) => props.theme.black_500};
+  }
+  p {
+    font-size: ${(props) => props.theme.mFont};
+    color: ${(props) => props.theme.black_500};
+    font-weight: 300;
+  }
+`
