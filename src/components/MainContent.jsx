0d05b340299842f08511c5559699154e1ee21074
Stalin <sta_vla411@hotmail.com>
2023-04-21 09:52:11 -0500

    remove background and change tag and colors

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import Button from './Button'
 import resumen from '../assets/resumen.webp'
-import url from '../assets/background_home.svg'
 function MainContent() {
   return (
     <SectionC>
-      <Background url={url} />
       <Container>
         <Content>
-          <h1>Toma el control de tus finanzas</h1>
+          <h2>Toma el control de tus finanzas</h2>
           <p>
             Nuestra app web de finanzas personales, diseñada para simplificar la
             gestión del dinero, permite llevar un seguimiento rápido y fácil de
@@ -29,42 +27,28 @@ function MainContent() {
 export default MainContent
 
 const SectionC = styled.section`
-  min-height: calc(100vh - 133px);
   position: relative;
-  padding: 4rem 6rem 2rem;
+  padding: 4rem 6rem;
   @media screen and (max-width: 1080px) {
-    padding: 3rem 3rem 2rem;
+    padding: 3rem;
   }
   @media screen and (max-width: 768px) {
-    padding: 1.5rem 1.5rem 2rem;
+    padding: 1.5rem;
   }
   @media screen and (max-width: 480px) {
-    padding: 1rem 1rem 2rem;
+    padding: 1rem;
   }
-`
-
-const Background = styled.div`
-  position: absolute;
-  background: url(${(props) => props.url});
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  width: 100%;
-  height: 100vh;
-  bottom: 0;
-  left: 0;
-  z-index: -1;
 `
 const Container = styled.article`
   display: flex;
-  gap: 2rem;
+  gap: 4rem;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   img {
     max-width: 40rem;
     border-radius: 12px;
-    box-shadow: #00000021 0px 1px 9px 0px;
+    border: 2px solid ${(props) => props.theme.black_500};
     @media screen and (max-width: 768px) {
       max-width: 100%;
     }
@@ -75,13 +59,14 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
   gap: 2rem;
-  h1 {
+  h2 {
     font-size: ${(props) => props.theme.xxlFont};
-    color: ${(props) => props.theme.white};
+    color: ${(props) => props.theme.black_500};
   }
   p {
     font-size: ${(props) => props.theme.mFont};
-    color: ${(props) => props.theme.white};
+    color: ${(props) => props.theme.black_500};
+    font-weight: 300;
   }
 `
 const ContainerBtn = styled.div`
