852f94476cad5524f088e5f9036a176c602f6e11
Stalin <sta_vla411@hotmail.com>
2024-01-31 21:05:29 -0500

    add svgs

---
+++ src/assets/icons/list-icons.jsx
@@ -0,0 +1,33 @@
+export const burgerIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='30'
+    height='30'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M4 6l16 0' />
+    <path d='M4 12l16 0' />
+    <path d='M4 18l16 0' />
+  </svg>
+)
+export const arrowIcon = (
+  <svg
+    width='28'
+    height='28'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M15 6l-6 6l6 6' />
+  </svg>
+)
