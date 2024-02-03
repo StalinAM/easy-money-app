b2369535a4eff60d5436ac1e3a3d4e860855f512
Stalin <sta_vla411@hotmail.com>
2024-02-03 00:06:30 -0500

    add new icons

--- src/assets/icons/list-icons.jsx
+++ src/assets/icons/list-icons.jsx
@@ -16,6 +16,43 @@ export const burgerIcon = (
     <path d='M4 18l16 0' />
   </svg>
 )
+export const editIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='20'
+    height='20'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4' />
+    <path d='M13.5 6.5l4 4' />
+  </svg>
+)
+export const deleteIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='20'
+    height='20'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M4 7l16 0' />
+    <path d='M10 11l0 6' />
+    <path d='M14 11l0 6' />
+    <path d='M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12' />
+    <path d='M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3' />
+  </svg>
+)
 export const closeIcon = (
   <svg
     xmlns='http://www.w3.org/2000/svg'
