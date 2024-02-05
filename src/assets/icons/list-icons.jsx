93e555e71aeaa50369f60ede70c51c1ca1c0ab78
Stalin <sta_vla411@hotmail.com>
2024-02-04 19:13:48 -0500

    add bew icons

--- src/assets/icons/list-icons.jsx
+++ src/assets/icons/list-icons.jsx
@@ -85,6 +85,24 @@ export const arrowIcon = (
     <path d='M15 6l-6 6l6 6' />
   </svg>
 )
+export const exclamationIcon = (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    strokeWidth='2'
+    stroke='currentColor'
+    fill='none'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+  >
+    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+    <path d='M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0' />
+    <path d='M12 9v4' />
+    <path d='M12 16v.01' />
+  </svg>
+)
 export const savingsIcon = (
   <svg
     height='90'
